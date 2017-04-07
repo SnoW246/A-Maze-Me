9286c204bcfbcacd25b0ccf8d092f7a814ca4149
Adrian Golias <SnoW246@azet.sk>
2017-04-07 20:29:49 +0100

    Applied physics to the sphere & materials to all objects. Created sphere script.
    Added rigidbody to the sphere, applied ice property to all maze walls
    and sphere to ensure neat and fast movement of the sphere. Applied plane
    under maze so that sphere doesnt fall through the maze. Applied colours.
    Added sphere script to reset sphere to spawn location when it falls off
    the maze.

---
+++ Assets/SphereScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour{
+    //
+    public GameObject plane;
+    public GameObject spawnPoint;
+
+	// Use this for initialization
+	void Start(){
+		
+	}// End of start function
+	
+	// Update is called once per frame
+	void Update(){
+        // 
+		if(transform.position.y < plane.transform.position.y - 10){
+            transform.position = spawnPoint.transform.position;
+        }// End of if
+	}// End of update function
+}// End of SphereScript Class
